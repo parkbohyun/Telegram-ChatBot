8db60e58cc0738ecf45f0b9bb17a8b9955cbacda
Bo._.H <47629333+parkbohyun@users.noreply.github.com>
2022-04-09 23:23:08 +0900

    Update Sc_m.py

--- Sc_m.py
+++ Sc_m.py
@@ -25,12 +25,13 @@ SE = scinfo[0].SD_SCHUL_CODE        # 학교코드
 scmeal = neis.mealServiceDietInfo(AE, SE, MLSV_YMD=D_day)         # 학교코드와 교육청 코드로 2022년 04월 01일의 급식 정보 요청
 meal = scmeal[0].DDISH_NM.replace("<br/>", "\n")  # 줄바꿈으로 만든뒤 출력
 
+# --------------------------------------------------------
+
 current = datetime.datetime.now()       # 오늘 날짜를 구함
 tomorrow = current + datetime.timedelta(days=1)     # 내일 날짜를 구함
 s_tm = str(tomorrow)        #str형태로 변경
 ST_day = s_tm[:4] + s_tm[5:7] + s_tm[8:10]      # 오늘 날짜를 "년월일" 형태로 구함
 STT_day = s_tm[:10]     # 오늘 날짜를 "년-월-일" 형태로 구함
-
 
 W2_day = ["화", "수", "목", "금" ,"월"]      # datetime 모듈을 이용하여 숫자로 구한 요일을 텍스트 형테로 변환
 WN2_day = datetime.datetime.today().weekday()    # datetime 모듈을 이용하여 오늘의 요일을 숫자로 구함
@@ -48,13 +49,14 @@ SE = scinfo[0].SD_SCHUL_CODE        # 학교코드
 scmeal_2 = neis.mealServiceDietInfo(AE, SE, MLSV_FROM_YMD=ST_day)
 meal_2 = scmeal_2[0].DDISH_NM.replace("<br/>", "\n")  # 줄바꿈으로 만든뒤 출력
 
+# --------------------------------------------------------
+
 # print("[ 성일정보고등학교 급식 ]"+"\n"+DD_day+" ("+(W_day[WN_day])+")\n[중식]")
 # print(meal)
 
 ## 텔레그램 봇
 token = "5151400483:AAHtsEuBlBF64ccaQiYf4-4Yceov0BFmIhM"
 M_1 = "5174190629" #5174190629 5271328836
-M_2 = "5271328836"
 
 # for i in bot.getUpdates():    CHAT_ID
 #     print(i.message)          CHAT_ID
@@ -76,19 +78,12 @@ def handler(update, context):
 
     # 사용자가 보낸 메세지가 "오늘급식"일 경우
     if user_text == "오늘급식": 
-        bot.send_message(chat_id=M_2, text="[ 성일정보고등학교 급식 ]"+"\n"+DD_day+" ("+(W_day[WN_day])+")\n[중식]\n"+meal)
-#         bot.send_message(chat_id=M_2, text="[ 성일정보고등학교 급식 ]"+"\n"+DD_day+" ("+(W_day[WN_day])+")\n[중식]\n"+meal)
-    
+        bot.send_message(chat_id=M_1, text="[ 성일정보고등학교 급식 ]"+"\n"+DD_day+" ("+(W_day[WN_day])+")\n[중식]\n"+meal)
+
     # 사용자가 보낸 메세지가 "내일급식"일 경우
     elif user_text == "내일급식":
-        bot.send_message(chat_id=M_2, text="[ 성일정보고등학교 급식 ]"+"\n"+STT_day+" ("+(W2_day[WN2_day])+")\n[중식]\n"+meal_2)
-#         bot.send_message(chat_id=M_2, text="[ 성일정보고등학교 급식 ]"+"\n"+STT_day+" ("+(W2_day[WN2_day])+")\n[중식]\n"+meal_2)
-
-    elif user_text == "!오늘급식":
-        bot.send_message(chat_id=M_1, text="[ 성일정보고등학교 급식 ]"+"\n"+DD_day+" ("+(W_day[WN_day])+")\n[중식]\n"+meal)
- 
-    elif user_text == "!내일급식":
         bot.send_message(chat_id=M_1, text="[ 성일정보고등학교 급식 ]"+"\n"+STT_day+" ("+(W2_day[WN2_day])+")\n[중식]\n"+meal_2)
+
 echo_handler = MessageHandler(Filters.text, handler)
 dispatcher.add_handler(echo_handler)
 
